3e1f7d43b90d5eedcb1584797742798975be231d
Caleb Anderson <buyananderson@gmail.com>
2021-07-19 10:40:28 +0200

    aSyle the side menu using sass

--- src/componentes/common/menu-side.jsx
+++ src/componentes/common/menu-side.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import MediaLink from "./medalink";
+import { Link } from "react-router-dom";
 
 const MenuSide = (props) => {
   return (
-    <aside className="menu-side">
+    <aside className="menu-side d-flex flex-column justify-content-evenly">
       <ul className="lang d-flex">
         <li>ENG</li>
         <li>FRA</li>
         <li>ESP</li>
       </ul>
       <ul className="menu flex-column justify-content-center d-flex">
-        <li>Home</li>
-        <li>Personalization</li>
-        <li>Services</li>
-        <li>Projects</li>
-        <li>Reviews</li>
-        <li>Contact</li>
+        <li>
+          <Link to="#">Home</Link>
+        </li>
+        <li>
+          <Link to="#">Personalization</Link>
+        </li>
+        <li>
+          <Link to="#">Services</Link>
+        </li>
+        <li>
+          <Link to="#">Projects</Link>
+        </li>
+        <li>
+          <Link to="#">Reviews</Link>
+        </li>
+        <li>
+          <Link to="#">Contact</Link>
+        </li>
       </ul>
       <MediaLink color="small-color" />
     </aside>
