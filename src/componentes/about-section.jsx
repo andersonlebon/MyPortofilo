8e9fab17f9b28dbff3f11a7f2a93a516ca4a3715
Caleb Anderson <buyananderson@gmail.com>
2021-07-25 18:54:46 +0200

    Add the about me information

--- src/componentes/about-section.jsx
+++ src/componentes/about-section.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const About = () => {
-  return <section>Hello there</section>;
+  return (
+    <section className="about-section">
+      <h3 className="">WHAT I DO</h3>
+      <div className="about-text">
+        <h4>About me</h4>
+        <p className="text-bold">
+          MY MISSION IS <span className="text-yellow"> DESIGN & DEVELOP </span>{" "}
+          THE BEST WEBSITES AROUND
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default About;
