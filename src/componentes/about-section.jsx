4c6e3babf3b86f8307bdc551cc5d845623655e8f
Caleb Anderson <buyananderson@gmail.com>
2021-07-25 22:18:41 +0200

    Add a library to isplay the progress bar

--- src/componentes/about-section.jsx
+++ src/componentes/about-section.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Progress } from "react-sweet-progress";
+import "react-sweet-progress/lib/style.css";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 const About = () => {
   return (
-    <section className="about-section">
+    <section className="about-section text-white d-flex j">
       <h3 className="">WHAT I DO</h3>
       <div className="about-text">
         <h4>About me</h4>
@@ -10,6 +13,55 @@ const About = () => {
           MY MISSION IS <span className="text-yellow"> DESIGN & DEVELOP </span>{" "}
           THE BEST WEBSITES AROUND
         </p>
+      </div>
+      <div className="about-progress text-small w-50">
+        <ul>
+          <li>
+            <span>Developement</span>
+            <ProgressBar
+              bgColor="#f3ca2f"
+              completed={60}
+              height="8px"
+              labelAlignment="outside"
+              labelColor="#9e9e9e"
+              labelSize="12"
+              transitionDuration="3s"
+            />
+          </li>
+          <li>
+            <ProgressBar
+              bgColor="#f3ca2f"
+              completed={60}
+              height="8px"
+              labelAlignment="outside"
+              labelColor="#9e9e9e"
+              labelSize="12"
+              transitionDuration="3s"
+            />
+          </li>
+          <li>
+            <ProgressBar
+              bgColor="#f3ca2f"
+              completed={60}
+              height="8px"
+              labelAlignment="outside"
+              labelColor="#9e9e9e"
+              labelSize="12"
+              transitionDuration="3s"
+            />
+          </li>
+          <li>
+            <ProgressBar
+              bgColor="#f3ca2f"
+              completed={60}
+              height="8px"
+              labelAlignment="outside"
+              labelColor="#9e9e9e"
+              labelSize="12"
+              transitionDuration="3s"
+            />
+          </li>
+        </ul>
       </div>
     </section>
   );
