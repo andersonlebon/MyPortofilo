6b2bbe37d92581669581a7b3f94bb8ea908de8b3
Caleb Anderson <buyananderson@gmail.com>
2021-08-01 22:20:34 +0200

    Add more styling to the text in the service section

--- src/componentes/services.jsx
+++ src/componentes/services.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Services = () => {
   return (
-    <section className="about-section text-white d-flex justify-content-center align-items-center">
+    <section className="services text-white d-flex justify-content-center p-3 align-items-center">
       <div className="aboutme d-flex align-items-start about-text w-50 p-5">
         <h3 className=" vertical-text">SERVICES</h3>
-        <div className="d-flex flex-column align-items-between">
+        <div className="s-text d-flex flex-column align-items-between">
           <h4>My Services</h4>
           <p className="text-bold">
             I LIKE <span className="text-yellow"> TO MAKE </span> THINGS
@@ -16,6 +16,8 @@ const Services = () => {
             <li>DEVELOPMENT</li>
             <li>DESIGN</li>
             <li>TECH SOLUTIONS</li>
+            <li>DEVELOPMENT</li>
+            <li>DESIGN</li>
           </ol>
         </div>
       </div>
